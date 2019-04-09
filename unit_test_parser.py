10ce23b79f9362c41f826ebd008eaf2a86ed20c2
AmandaKatherine <47579399+AmandaKatherine@users.noreply.github.com>
2019-04-09 11:41:19 -0500

    Add files via upload
    i am still working on it but this is the practice code I have been working with

---
+++ unit_test_parser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this is practicing unit testing so i can do it for the sample sheet parser. 
+#i hope to have this up and running by friday night 
+#sorry that this isnt 
+
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
+
