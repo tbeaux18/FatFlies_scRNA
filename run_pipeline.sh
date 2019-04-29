1a7a7f00426876b188cdb013e967e7a6497085d4
tbaker18 <tim.baker@tempus.com>
2019-04-29 18:27:03 -0500

    added command to remove container when complete

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -10,13 +10,10 @@ cd docker && ./build_me.sh && cd ../
 parentdir="$(dirname "$mount_dir")"
 
 docker run \
+  --name scrna_pipeline \
   --user $(id -u):$(id -g) \
   -d \
   -v "${parentdir}":/pipeline \
   ubuntur35:pipeline
 
-
-# mv the necessary output files one directory up
-# grab relevant mapping results
-# check the yaml log file to make sure output code is [1] 0 indicates nothing is wrong
-# once the genome index is built, move one directory up so resources are not wasted on redoing it
+docker run --rm scrna_pipeline
